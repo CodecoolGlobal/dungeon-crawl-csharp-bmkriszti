050d0066d31631785dd12dc2f40bb2563025a5a8
Akos Szajbert <kisszajbert@gmail.com>
2024-05-22 10:30:24 +0200

    Dinamic wall numbers

--- Maps/Map.cs
+++ Maps/Map.cs
@@ -27,6 +27,7 @@ namespace DungeonCrawl.Maps
             UserControlledObject = new Player(_mapSurface.Surface.Area.Center, _mapSurface);
             _mapObjects.Add(UserControlledObject);
 
+            CreateMultipleObjects(mapWidth*mapHeight/10, CreateWall);
             InitializeObjects();
         }
 
@@ -36,6 +37,7 @@ namespace DungeonCrawl.Maps
             CreateMultipleObjects(5, CreateMonster);
             CreateMultipleObjects(2, CreateKey); 
             CreateMultipleObjects(2, CreateSword);
+            
         }
 
         private void CreateMultipleObjects(int count, Action createObject)
@@ -130,6 +132,22 @@ namespace DungeonCrawl.Maps
 
                 GameObject monster = new Monster(randomPosition, _mapSurface);
                 _mapObjects.Add(monster);
+                break;
+            }
+        }
+        
+        private void CreateWall()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Point randomPosition = new Point(Game.Instance.Random.Next(0, _mapSurface.Surface.Width),
+                    Game.Instance.Random.Next(0, _mapSurface.Surface.Height));
+
+                bool foundObject = _mapObjects.Any(obj => obj.Position == randomPosition);
+                if (foundObject) continue;
+
+                GameObject wall = new Wall(randomPosition, _mapSurface);
+                _mapObjects.Add(wall);
                 break;
             }
         }
