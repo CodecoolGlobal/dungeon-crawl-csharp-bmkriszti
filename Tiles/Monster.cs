b2fadc2e1bbdf1a4b5b9f6d4cd7c7e44c20f17c8
Szekely <szekelyboti1@gmail.com>
2024-05-23 12:12:04 +0200

    dmg display inventory tweak

--- Tiles/Monster.cs
+++ Tiles/Monster.cs
@@ -1,15 +1,20 @@
-﻿using DungeonCrawl.Maps;
+﻿using System;
+using DungeonCrawl.Maps;
 using SadConsole;
 using SadRogue.Primitives;
-using Console = System.Console;
 
 namespace DungeonCrawl.Tiles
 {
     public abstract class Monster : GameObject
     {
+        public int Hp { get; protected set; }
+        public int AttackDamage { get; protected set; }
+
         public Monster(ColoredGlyph appearance, Point position, IScreenSurface hostingSurface, int hp, int attackDamage)
-            : base(appearance, position, hostingSurface, hp, attackDamage)
+            : base(appearance, position, hostingSurface,hp,attackDamage)
         {
+            Hp = hp;
+            AttackDamage = attackDamage;
         }
 
         protected override bool Touched(GameObject source, Map map)
@@ -18,20 +23,37 @@ namespace DungeonCrawl.Tiles
             {
                 if (Hp > 0)
                 {
+                    // Player and monster take damage
                     Hp -= source.AttackDamage;
                     source.Hp -= AttackDamage;
-                    Console.WriteLine($"Player {source.Hp},{source.AttackDamage}");
-                    Console.WriteLine($"Monster {Hp}");
+
+                    // Notify subscribers that the monster's state has changed
+                    OnMonsterStateChanged();
+                    
+                    // Check if monster is still alive
                     return false;
                 }
 
                 if (Hp <= 0)
                 {
                     map.RemoveMapObject(this);
+
+                    // Notify subscribers that the monster's state has changed
+                    OnMonsterStateChanged();
+                    
                     return true;
                 }
             }
             return false;
+        }
+
+        // Define an event to notify when the monster's state changes
+        public static event EventHandler MonsterStateChanged;
+
+        // Method to raise the MonsterStateChanged event
+        protected virtual void OnMonsterStateChanged()
+        {
+            MonsterStateChanged?.Invoke(this, EventArgs.Empty);
         }
     }
 }
--- Ui/RootScreen.cs
+++ Ui/RootScreen.cs
@@ -1,77 +1,123 @@
-﻿using DungeonCrawl.Maps;
+﻿using System;
+using System.Collections.Generic;
+using DungeonCrawl.Maps;
+using DungeonCrawl.Tiles;
 using SadConsole;
 using SadConsole.Input;
 using SadRogue.Primitives;
 
-namespace DungeonCrawl.Ui;
-
-/// <summary>
-/// Class <c>RootScreen</c> provides parent/child, components, and position.
-/// </summary>
-public class RootScreen : ScreenObject
+namespace DungeonCrawl.Ui
 {
-    private Map _map;
-    private SadConsole.Console _inventoryConsole;
-
-    /// <summary>
-    /// Constructor.
-    /// </summary>
-    public RootScreen()
+    public class RootScreen : ScreenObject
     {
-        _map = new Map(Game.Instance.ScreenCellsX, Game.Instance.ScreenCellsY - 5);
+        private Map _map;
+        private SadConsole.Console _inventoryConsole;
+        private SadConsole.Console _statsConsole; // Add a stats console
 
-        Children.Add(_map.SurfaceObject);
+        public RootScreen()
+        {
+            _map = new Map(Game.Instance.ScreenCellsX, Game.Instance.ScreenCellsY - 5);
+
+            Children.Add(_map.SurfaceObject);
+            
+            _inventoryConsole = new SadConsole.Console(Game.Instance.ScreenCellsX, 5);
+            _inventoryConsole.Position = new Point(0, Game.Instance.ScreenCellsY - 5);
+            _inventoryConsole.DefaultBackground = Color.Black;
+            _inventoryConsole.Clear();
+
+            Children.Add(_inventoryConsole);
+
+            _statsConsole = new SadConsole.Console(Game.Instance.ScreenCellsX, 5); // Initialize stats console
+            _statsConsole.Position = new Point(0, Game.Instance.ScreenCellsY - 10); // Adjust position as needed
+            _statsConsole.DefaultBackground = Color.Black;
+            _statsConsole.Clear();
+
+            Children.Add(_statsConsole); // Add stats console to children
+
+            // Subscribe to the MonsterStateChanged event
+            Monster.MonsterStateChanged += OnMonsterStateChanged;
+        }
+        private void OnMonsterStateChanged(object sender, EventArgs e)
+        {
+            // Ensure the sender is a Monster object
+            if (sender is Monster monster)
+            {
+                // Update character stats display for the changed monster
+                UpdateCharacterStatsDisplay(monster);
+            }
+        }
         
-        _inventoryConsole = new SadConsole.Console(Game.Instance.ScreenCellsX, 5);
-        _inventoryConsole.Position = new Point(0, Game.Instance.ScreenCellsY - 5);
-        _inventoryConsole.DefaultBackground = Color.Black;
-        _inventoryConsole.Clear();
-
-        Children.Add(_inventoryConsole);
-    }
-    
-    public void UpdateInventoryDisplay()
-    {
-        _inventoryConsole.Clear();
-        var items = _map.UserControlledObject.Inventory;
-        for (int i = 0; i < items.Count; i++)
+        public void UpdateInventoryDisplay()
         {
-            _inventoryConsole.Print(0, i, $"{items[i].GetType().Name}");
-        }
-        _inventoryConsole.IsDirty = true; 
-    }
+            _inventoryConsole.Clear();
 
-    /// <summary>
-    /// Processes keyboard inputs.
-    /// </summary>
-    /// <param name="keyboard"></param>
-    /// <returns></returns>
-    public override bool ProcessKeyboard(Keyboard keyboard)
-    {
-        bool handled = false;
+            var items = _map.UserControlledObject.Inventory;
+            Dictionary<string, int> itemCounts = new Dictionary<string, int>();
+            foreach (var item in items)
+            {
+                string itemName = item.GetType().Name;
+                if (itemCounts.ContainsKey(itemName))
+                {
+                    itemCounts[itemName]++;
+                }
+                else
+                {
+                    itemCounts[itemName] = 1;
+                }
+            }
+            int row = 0;
+            foreach (var kvp in itemCounts)
+            {
+                _inventoryConsole.Print(0, row++, $"{kvp.Key} x{kvp.Value}");
+            }
 
-        if (keyboard.IsKeyPressed(Keys.Up))
-        {
-            _map.UserControlledObject.Move(_map.UserControlledObject.Position + Direction.Up, _map);
-            handled = true;
-        }
-        else if (keyboard.IsKeyPressed(Keys.Down))
-        {
-            _map.UserControlledObject.Move(_map.UserControlledObject.Position + Direction.Down, _map);
-            handled = true;
+            _inventoryConsole.IsDirty = true;
         }
 
-        if (keyboard.IsKeyPressed(Keys.Left))
+        public void UpdateCharacterStatsDisplay(Monster monster)
         {
-            _map.UserControlledObject.Move(_map.UserControlledObject.Position + Direction.Left, _map);
-            handled = true;
-        }
-        else if (keyboard.IsKeyPressed(Keys.Right))
-        {
-            _map.UserControlledObject.Move(_map.UserControlledObject.Position + Direction.Right, _map);
-            handled = true;
+            _statsConsole.Clear();
+
+            var player = _map.UserControlledObject;
+
+            // Display current HP and attack of the player
+            _statsConsole.Print(0, 0, $"Player HP: {player.Hp}");
+            _statsConsole.Print(0, 1, $"Player Attack: {player.AttackDamage}");
+
+            // Display current HP and attack of the monster
+            _statsConsole.Print(0, 3, $"Monster HP: {monster.Hp}");
+            _statsConsole.Print(0, 4, $"Monster Attack: {monster.AttackDamage}");
+
+            _statsConsole.IsDirty = true;
         }
 
-        return handled;
+        public override bool ProcessKeyboard(Keyboard keyboard)
+        {
+            bool handled = false;
+
+            if (keyboard.IsKeyPressed(Keys.Up))
+            {
+                _map.UserControlledObject.Move(_map.UserControlledObject.Position + Direction.Up, _map);
+                handled = true;
+            }
+            else if (keyboard.IsKeyPressed(Keys.Down))
+            {
+                _map.UserControlledObject.Move(_map.UserControlledObject.Position + Direction.Down, _map);
+                handled = true;
+            }
+
+            if (keyboard.IsKeyPressed(Keys.Left))
+            {
+                _map.UserControlledObject.Move(_map.UserControlledObject.Position + Direction.Left, _map);
+                handled = true;
+            }
+            else if (keyboard.IsKeyPressed(Keys.Right))
+            {
+                _map.UserControlledObject.Move(_map.UserControlledObject.Position + Direction.Right, _map);
+                handled = true;
+            }
+
+            return handled;
+        }
     }
 }
